d55219b6df04dc760761161f1e47519956ae6a2a
Lucas V. Oliveira <lucas.o@live.com>
2016-01-08 10:20:03 -0200

    prog

--- htmls/navbar.html.php
+++ htmls/navbar.html.php
@@ -4,6 +4,12 @@
     <div class="navbar-header">
       <a class="navbar-brand" href="#">Navigatte</a>
     </div>
+    <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          	<input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+     </form>
 
     <?php include 'userbutton.html.php'; ?>
 
